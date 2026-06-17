e1a1a7bd2f0efed8fdd7a62758128c7e5e0c58b5
Musca Mihail <muscamihaill@gmail.com>
2026-06-17 14:41:17 +0300

    feat: added package filtering to search command

--- core/src/main/java/quill/command/commands/CommandSearch.java
+++ core/src/main/java/quill/command/commands/CommandSearch.java
@@ -2,6 +2,8 @@ package quill.command.commands;
 
 import java.util.List;
 
+import fluff.commander.argument.ArgumentBuilder;
+import fluff.commander.argument.IArgument;
 import fluff.commander.command.CommandArguments;
 import fluff.commander.command.CommandException;
 import quill.Quill;
@@ -14,14 +16,32 @@ import quill.remote.RemotePackage;
 import quill.text.Text;
 
 public class CommandSearch extends Command {
+	
+	public static final IArgument<String> ARG_PACKAGE = ArgumentBuilder.String("--package", "-p").inline().build();
 
 	public CommandSearch() {
 		super("search");
 	}
 
 	@Override
+	public void init() {
+		argument(ARG_PACKAGE);
+	}
+	
+	@Override
 	public int onAction(QCommander c, CommandSource source, CommandArguments args) throws CommandException {
-		List<RemotePackage> packages = Quill.INSTANCE.remoteRepositories.find(TagCriteria.of(TagCriteria.ANY));
+		String argPackage = args.get(ARG_PACKAGE);
+		TagCriteria criteria = argPackage != null ? TagCriteria.of(argPackage) : TagCriteria.of(TagCriteria.ANY);
+		if (criteria == null) {
+			source.print(Text.fg(Text.RED).s("Invalid package."));
+			return FAIL;
+		}
+		
+		List<RemotePackage> packages = Quill.INSTANCE.remoteRepositories.find(criteria);
+		
+		if (packages.isEmpty()) {
+			source.print(Text.fg(Text.CYAN).s("No packages found."));
+		}
 
 		for (RemotePackage p : packages) {
 			Release release = Release.of(p);
